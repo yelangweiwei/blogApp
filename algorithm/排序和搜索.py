984863f2cfc553fca817c5711ccd1389da32447c
Administrator <528150249@qq.com>
2021-02-18 17:32:20 +0800

    练习程序上传

--- algorithm/排序和搜索.py
+++ algorithm/排序和搜索.py
@@ -163,5 +163,221 @@ def quick_sort_pra():
 
 
 
+
+
+
+
+'''
+二叉树：
+    二叉树的性质：
+        1，在二叉树的第i层上至多有2^（i-1）个节点（i>0）
+        2,深度为k的二叉树至多有2^k-1个节点（k>0）
+        3,对于任意一棵二叉树，如果其叶节点数为N0,而度为2的节点总数为N2,则N0=N2+1
+        4，具有n个节点的完全二叉树的深度必为log2(n+1)
+        5,对于完全二叉树，从上至下，从左至右，编号为i的节点，其左孩子的编号为2*i,右孩子的编号为2*i+1,其双亲的编号必为i/2(i=1除外)
+'''
+
+
+class Node(object):
+    def __init__(self,item):
+        self.elem =item
+        self.lchild = None
+        self.rchild = None
+
+
+class Tree(object):
+    def __init__(self,root=None):
+        self.root = root
+
+    def add_item(self,item):
+        node = Node(item)
+        if self.root == None:
+            self.root = node
+            return
+        queue = []
+        queue.append(self.root)
+        while queue:
+            curr_node = queue.pop(0) #提取最左边的值
+            if curr_node.lchild is None:
+                curr_node.lchild= node
+                return
+            else:
+                queue.append(curr_node.lchild)
+
+            if curr_node.rchild is None:
+                curr_node.rchild = node
+                return
+            else:
+                queue.append(curr_node.rchild)
+
+    def breadth_travel(self):
+        '''广度遍历'''
+        if self.root is None:
+            return
+        queue = [self.root]
+        while queue:
+            curr_node = queue.pop(0)
+            print(curr_node.elem,end=',')
+            if curr_node.lchild is not None:
+                queue.append(curr_node.lchild)
+            if curr_node.rchild is not None:
+                queue.append(curr_node.rchild)
+
+    def preorder(self,node):
+        '''
+        深度遍历
+        先序遍历  根，左，右
+        采用递归完成
+        :return:
+        '''
+        if node is None:
+            return
+        #遍历头节点
+        print(node.elem,end=',')
+        #左节点
+        self.preorder(node.lchild)
+        #右节点
+        self.preorder(node.rchild)
+    def midorder(self,node):
+
+        '''
+         中序遍历：先遍历左节点，在遍历根节点，后遍历右节点
+        :param node:
+        :return:
+        '''
+        if node is None:
+            return
+        self.midorder(node.lchild)
+        print(node.elem,end=',')
+        self.midorder(node.rchild)
+
+    def postOrder(self,node):
+        '''
+         后序遍历， 先遍历左左节点，在遍历右节点，最后根节点
+        :param node:
+        :return:
+        '''
+
+        if node is None:
+            return
+        self.postOrder(node.lchild)
+        self.postOrder(node.rchild)
+        print(node.elem,end=',')
+
+
+def tree_pra():
+    tree = Tree()
+    for i in range(10):
+        tree.add_item(i)
+
+    # 广度遍历
+    tree.breadth_travel()
+    print('\n')
+    # 先序遍历
+    tree.preorder(tree.root)
+    print('\n')
+    # 中序遍历
+    tree.midorder(tree.root)
+    print('\n')
+    # 后续遍历
+    tree.postOrder(tree.root)
+
+
+def sift_big(li,low,high):
+    '''
+    堆排序，特殊的完全二叉树
+    大根堆：一棵完全二叉树，任何一个节点都比孩子节点大，排出来是升序的的
+    小根堆： 满足任何一个节点都比孩子节点小。排出来是降序的
+    当根节点的左右子树都是堆时，可以通过一次向下的调整来将其变换成一个堆
+    li 表示树，low:表示树根，high表示最后一个节点的位置
+
+    时间复杂度：O(nlog2N)
+    https://www.cnblogs.com/jingmoxukong/p/4303826.html
+    '''
+    tmp = li[low]
+    i = low  #指向根
+    j = 2*i+1 #j指向两个孩子
+    while j<=high: #当j>high，也就是到了子节点时，跳出
+        if j+1<=high and li[j]<li[j+1]:#完全二叉树，左子树也要小于右子树,指向右孩子
+            j=j+1
+        if li[j]>tmp:
+            li[i] = li[j]
+            i=j
+            j = i*2+1
+        else:
+            break  #子节点比当前值小
+    li[i]=tmp
+
+
+def sift_small(li,low,high):
+    '''
+    堆排序，特殊的完全二叉树
+    大根堆：一棵完全二叉树，任何一个节点都比孩子节点大，排出来是升序的的
+    小根堆： 满足任何一个节点都比孩子节点小。排出来是降序的
+    当根节点的左右子树都是堆时，可以通过一次向下的调整来将其变换成一个堆
+    li 表示树，low:表示树根，high表示最后一个节点的位置
+
+    时间复杂度：O(nlog2N)
+    https://www.cnblogs.com/jingmoxukong/p/4303826.html
+
+    这个主要用在前k个值的排序
+    '''
+    tmp = li[low]
+    i = low  #指向根
+    j = 2*i+1 #j指向两个孩子
+    while j<=high: #当j>high，也就是到了子节点时，跳出
+        if j+1<=high and li[j]>li[j+1]:#完全二叉树，左子树也要小于右子树,指向右孩子
+            j=j+1
+        if li[j]<tmp:
+            li[i] = li[j]
+            i=j
+            j = i*2+1
+        else:
+            break  #子节点比当前值小
+    li[i]=tmp
+
+
+def heap_sort(li):
+    n = len(li)
+    #构造堆,初始化堆,构造大顶堆
+    for low in range(n//2-1,-1,-1):
+        sift_small(li,low,n-1)  #开始的位置和最后一个数据的位置
+    print(li)
+    print('----------------------')
+    #将大顶堆的值放在叶子节点处,最后的位置不断的向前移动
+    for high in range(n-1,-1,-1):
+        li[0],li[high]=li[high],li[0]  #退休棋子调整
+        sift_small(li,0,high-1)
+    print(li)
+
+def heap_pra():
+    alist = [2, 3, 6,4, 7,34, 56, 89, 90]
+    heap_sort(alist)
+
+#求前k个最大的，或者最小的值
+def heap_k_pra():
+    blist = [2, 3, 6, 4,34, 56, 89, 90,5,7,47]
+    k =11
+    alist= blist[:k]
+    #初始化堆
+    for low in range(k//2-1,-1,-1):
+        sift_small(alist,low,k-1)
+    #此时堆顶是最小的值
+    for data in blist[k:]:
+        if data>alist[0]:
+            alist[0]=data
+            sift_small(alist,0,k-1)
+
+    #将堆顶的值进行输出
+    for high in range(k-1,-1,-1):
+        alist[0],alist[high]=alist[high],alist[0]
+        sift_small(alist,0,high-1)
+    print(alist)
+
+
+
+
+
 if __name__=='__main__':
-    quick_sort_pra()
+    heap_k_pra()
+
